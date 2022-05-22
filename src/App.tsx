0afeb700b3ac1aa29cf0dc5a0be7e779bf971455
Riley <riley.gabrielson@tcnbroadcasting.com>
2022-05-22 12:47:44 -0600

    removing create react app bloat

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import "./App.css";
+import React, { CSSProperties } from "react";
 import { Network } from "./react/network/Network";
+
+const style: CSSProperties = {
+  backgroundColor: "#282c34",
+  minHeight: "100vh",
+  fontSize: "calc(10px + 2vmin)",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div style={{ textAlign: "center" }}>
+      <header style={style}>
         <Network />
       </header>
     </div>
